a0c865ffd09553015174846ed32e6ec65d791a64
ccwanggl <tswanggl@gmail.com>
2021-03-22 20:13:27 +0800

    Create glfw window   1. add_subdirectory   2. target_include_directories   3. target_link_directories   4. target_link_libraries

--- homework_1/task_2/src/main.cpp
+++ homework_1/task_2/src/main.cpp
@@ -4,6 +4,7 @@
 // Copyright (c) 2019 Ignacio Vizzo, all rights reserved
 #include <iostream>
 
+#include "GLFW/glfw3.h"
 #include "ipb_arithmetic.hpp"
 
 using std::cout;
@@ -14,6 +15,26 @@ int main() {
   const float y = 2.0;
   cout << "The sum of " << x << "+" << y << " = " << MySum(x, y) << endl
        << "The sub of " << x << "-" << y << " = " << MySubtract(x, y) << endl;
+
+  GLFWwindow *window;
+
+  if (!glfwInit()) {
+    fprintf(stderr, "Failed to initialize GLFW\n");
+    exit(EXIT_FAILURE);
+  }
+
+  window = glfwCreateWindow(300, 300, "Gears", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 
   return 0;
 }
